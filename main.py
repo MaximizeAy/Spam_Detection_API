f3062ffc3a61b2746e723ce8ecd20b1b5688f294
MaximizeAy <ayomide.daniel.adeosun@gmail.com>
2026-07-07 11:08:18 +0100

    Updated render url

--- main.py
+++ main.py
@@ -122,6 +122,7 @@ app.add_middleware(
         "http://localhost:3000",   # CRA default
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
+        "https://spam-detection-api-574s.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
